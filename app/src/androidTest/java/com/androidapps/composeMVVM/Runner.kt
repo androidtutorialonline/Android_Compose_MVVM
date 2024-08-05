55a84bd479dbb97349d049f01fd3b170764d9579
Android Tutorial <firojandroid@gmail.com>
2024-08-05 12:19:53 +0530

    updated code

--- app/src/androidTest/java/com/androidapps/composeMVVM/Runner.kt
+++ app/src/androidTest/java/com/androidapps/composeMVVM/Runner.kt
@@ -4,9 +4,6 @@ import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltTestApplication
-import io.cucumber.junit.Cucumber
-import io.cucumber.junit.CucumberOptions
-import org.junit.runner.RunWith
 
 /*@RunWith(Cucumber::class)
 @CucumberOptions(
